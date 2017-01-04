fd4ce50a5670463f97e7809252da7ad3dcac04ce
Mariah Tree <mariah@filestack.com>
2017-01-04 11:56:07 -0600

    notes template

---
+++ components/NotesList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="notes-list">
+
+    <div id="list-header">
+      <h2> Notes |  Coligo</h2>
+      <div class="btn-group btn-group-justified" role="group">
+
+        <div class="btn-group" role="group">
+          <button type="button" class="btn btn-default" @click="show = 'all'" :class="{active: show === 'all'}">
+          All Notes
+          </button>
+        </div>
+
+        <div class="btn-group" role="group">
+          <button type="button" class="btn btn-default" @click="show = 'favorite'" :class="{active: show === 'favorite'}">
+          Favorite
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="list-group">
+        <a v-for="note in filteredNotes" class="list-group-item" href="#" :class=""{active: activeNote === note}" @click="updateActiveNote(note)">
+          <h4 class="list-group-item-heading">
+            {{note.text.trim().substring(0, 30)}}
+          </h4>
+        </a>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { updateActiveNote } from '../vuex/actions'
+
+export default {
+  data () {
+    return {
+      show: 'all'
+    }
+  },
+  vuex: {
+    getters: {
+      notes: state => state.notes,
+      activeNote: state => state.activeNote
+    },
+    actions: {
+      updateActiveNote
+    }
+  },
+  computed: {
+    filteredNotes () {
+      if (this.show === 'all'){
+        return this.notes
+      } else if (this.show === 'favorite') {
+        return this.notes.filter(note => note.favorite)
+      }
+    }
+  }
+}
+</script>
